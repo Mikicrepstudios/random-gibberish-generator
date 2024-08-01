86850e55cd00593d07824312d1ae96d1e6c9c4a9
applearon <aronseshan@gmail.com>
2024-08-01 09:05:51 -0400

    Improve search.sh

--- search.sh
+++ search.sh
@@ -1,3 +1,8 @@
+#!/usr/bin/env bash
 # Replace string in "" with word you want to find in randomly generated file and random with filename
-grep -in "miki" random
+if [ -z $2 ]; then
+    echo "Usage: $0 file str"
+    exit 1
+fi
+grep -in "$2" $1
 
