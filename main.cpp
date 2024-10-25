f08ab77cb12ba57bdbb11d39b1fbd4677b9f6ba8
Mikicrep <mikicrepoffical@gmail.com>
2024-10-25 21:24:17 +0200

    1.3

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <cstdlib>
 #include <ctime>
+#include <string>
 
 std::string generateRandomWord(int length) {
     const std::string charSet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
@@ -26,21 +27,54 @@ void generateAndSaveWords(const std::string& filename, int wordLength, int numbe
     outFile.close();
 }
 
-int main() {
+void showHelp() {
+    std::cout << "Usage: program [-f filename] [-l wordLength] [-n numberOfWords] [-h]\n"
+              << "Options:\n"
+              << "  -f filename     Specify the output filename\n"
+              << "  -l wordLength   Specify the length of each word\n"
+              << "  -n numberOfWords Specify the number of words to generate\n"
+              << "  -h              Show this help message\n";
+}
+
+int main(int argc, char* argv[]) {
     srand(static_cast<unsigned>(time(0))); // Seed the random number generator
 
-    int wordLength;
-    int numberOfWords;
+    int wordLength = 0;
+    int numberOfWords = 0;
     std::string filename;
 
-    std::cout << "Enter the length of each word: ";
-    std::cin >> wordLength;
+    for (int i = 1; i < argc; ++i) {
+        std::string arg = argv[i];
 
-    std::cout << "Enter the number of words to generate: ";
-    std::cin >> numberOfWords;
+        if (arg == "-f" && i + 1 < argc) {
+            filename = argv[++i];
+        } else if (arg == "-l" && i + 1 < argc) {
+            wordLength = std::stoi(argv[++i]);
+        } else if (arg == "-n" && i + 1 < argc) {
+            numberOfWords = std::stoi(argv[++i]);
+        } else if (arg == "-h") {
+            showHelp();
+            return 0;
+        } else {
+            std::cerr << "Unknown option: " << arg << std::endl;
+            showHelp();
+            return 1;
+        }
+    }
 
-    std::cout << "Enter the filename to save the words: ";
-    std::cin >> filename;
+    // Validate input if not provided
+    if (filename.empty()) {
+        std::cout << "Enter the filename to save the words: ";
+        std::cin >> filename;
+    }
+    if (wordLength == 0) {
+        std::cout << "Enter the length of each word: ";
+        std::cin >> wordLength;
+    }
+    if (numberOfWords == 0) {
+        std::cout << "Enter the number of words to generate: ";
+        std::cin >> numberOfWords;
+    }
 
     generateAndSaveWords(filename, wordLength, numberOfWords);
 
@@ -48,4 +82,3 @@ int main() {
 
     return 0;
 }
-
